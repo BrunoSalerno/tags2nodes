ca4cb2c60298054245e909c3d8a6b52c99ca0984
bruno <br.salerno@gmail.com>
2014-12-22 22:48:39 -0300

    0.0.1

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ tags2nodes
 ==========
 
 An OSM tool for attaching relation tags to the member nodes
+
+Options
+-------
+
+ -f      input osm.pbf file
+ -u      postgres user
+ -p      postgres password
+ -h      postgres host
+ -d      postgres db name
+ -c      osm relation condition json. I.e.: {'route':'subway'}'
+ -r      role of members of relation to be filtered. I.e.: stop
+ -t      [optional] postgres output table name. Default: tags2nodes
+ -j      [optional] output projection. Default: WGS84 (4326)
+ --json  [optional] json tags field postgres format. Only valid for Postgres >= 9.3. Default: text
+ --jsonb [optional] jsonb tags field postgres format (faster!). Only valid for Postgres >= 9.4. Default: text
+	
+License
+-------
+
+MIT.
---
+++ tags2nodes.js
@@ -0,0 +1,86 @@
+var osmread=require('osm-read');
+var pg = require('pg').native;
+var Q = require('q');
+var argv = require('minimist')(process.argv.slice(2));
+var postgres=require('./lib/postgres');
+var osm=require('./lib/osm');
+
+if (argv["help"] || argv["h"]) {
+	console.log(" tags2nodes arguments ");
+	console.log(" -f      input osm.pbf file");
+	console.log(" -u      postgres user");
+	console.log(" -p      postgres password");
+	console.log(" -h      postgres host");
+	console.log(" -d      postgres db name");
+  console.log(" -c      osm relation condition json. I.e.: {'route':'subway'}'");
+  console.log(" -r      role of members of relation to be filtered. I.e.: stop"); 
+	console.log(" -t      [optional] postgres output table name. Default: tags2nodes");
+	console.log(" -j      [optional] output projection. Default: WGS84 (4326)");
+	console.log(" --json  [optional] json tags field postgres format. Only valid for Postgres >= 9.3. Default: text");
+	console.log(" --jsonb [optional] jsonb tags field postgres format (faster!). Only valid for Postgres >= 9.4. Default: text");
+	return;
+};
+
+if (!argv["f"] || !argv["u"] || !argv["p"] || !argv["h"] || !argv["d"] || !argv["r"] || !argv["c"]) {
+	console.log("Arguments missing");
+	return;
+};
+
+var FILE_PATH = argv["f"];
+
+var connection={
+			username: argv["u"],
+			password: argv["p"],
+			host: argv["h"],
+			db: argv["d"]
+	}
+
+var table = argv["t"] || "tags2nodes";
+var proj = (argv["j"])? parseInt(argv["j"]) : 4326;	
+
+var connection_string = "postgres://"+connection.username+":"+connection.password+"@"+connection.host+"/"+connection.db;
+
+var db= new postgres.Query;
+
+osm = new osm.Osm(osmread,FILE_PATH, Q);
+
+var relation_conditions = JSON.parse(argv["c"]);
+var role = argv["r"];
+
+var tags_type='text'; 
+if (argv["json"]) tags_type='json';
+if (argv["jsonb"]) tags_type='jsonb';
+
+console.log('Fetching relations...');
+	
+osm.parse(relation_conditions,'relation', function(relations){				
+	var nodes={};
+	relations.forEach (function(relation){
+		relation.members.forEach(function(member){
+			if (member.role==role){
+					if (!nodes[member.ref]){
+						nodes[member.ref]={member:member,relations:[]}						
+					}
+					nodes[member.ref].relations.push(relation);
+			}
+		});
+	});
+	console.log("Fetching and saving nodes...");
+	
+	pg.connect(connection_string, function(err, client, done) {
+		
+		if (err) {
+			return console.error('Error fetching client from pool', err);
+		}
+  
+		db.set_client(client,proj);
+		osm.fetch_and_save_nodes(table,db,nodes,tags_type, function(number_rows_inserted){
+			console.log(number_rows_inserted + " nodes inserted into " + table + ".");
+			done();
+		});
+	});
+	
+});
+
+
+
